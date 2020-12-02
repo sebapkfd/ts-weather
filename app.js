9dda8f2ab4c9b6170a23552ad5a61e2044d5c24c
Seba Soto <sebastian.pkfd@gmail.com>
2020-12-01 22:36:28 -0300

    Added icon, feels like, condition and date time to info

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const contDiv = document.getElementById('content');
 
+
+
 async function getWeather(cityName){
     try {
         const response = await fetch(
             `http://api.weatherapi.com/v1/current.json?key=a5f5d927bbc64246a51205628200112&q=${cityName}`, 
             {mode: 'cors'})
         const data = await response.json();
-        const weather = { temp: `${data.current.temp_c}°`, humidity: `${data.current.humidity}%` ,country: data.location.country, city: data.location.name};
+        console.log(data);
+        const weather = { 
+            temp: `${data.current.temp_c}°`,
+            condition: data.current.condition.text,
+            humidity: `${data.current.humidity}%`,
+            country: data.location.country,
+            city: data.location.name,
+            feelsLike: `${data.current.feelslike_c}°`,
+            icon: `http:${data.current.condition.icon}`,
+            dateTime: data.location.localtime
+        };
+        console.log(weather);
         return weather;
     } catch (error) {
         console.log(error);
@@ -20,7 +33,35 @@ function displayinfo(info){
 
     const infoDiv = document.createElement('div');
     infoDiv.className = 'infoDiv';
-    infoDiv.innerText = `Location: ${info.city}, ${info.country} \n Temp: ${info.temp} \n Humidity: ${info.humidity}`;
+
+    const locationDiv = document.createElement('div');
+    locationDiv.setAttribute('id', 'locationDiv');
+    locationDiv.innerText = `${info.city}, ${info.country}`;
+
+    const iconDiv = document.createElement('div');
+    iconDiv.setAttribute('id', 'iconDiv');
+
+    const img = document.createElement('img');
+    img.src = info.icon;
+
+    const tempDiv = document.createElement('div');
+    tempDiv.setAttribute('id', 'tempDiv');
+    tempDiv.innerText = `${info.temp}`;
+
+    const humidityDiv = document.createElement('div');
+    humidityDiv.setAttribute('id', 'humidityDiv');
+    humidityDiv.innerText = `Humidity: ${info.humidity}`;
+
+    const feelsLikeDiv = document.createElement('div');
+    feelsLikeDiv.setAttribute('id', 'feelsLikeDiv');
+    feelsLikeDiv.innerText = `Feels like: ${info.feelsLike}`;
+
+    infoDiv.appendChild(locationDiv);
+    iconDiv.appendChild(img);
+    infoDiv.appendChild(iconDiv);
+    infoDiv.appendChild(tempDiv);
+    infoDiv.appendChild(humidityDiv);
+    infoDiv.appendChild(feelsLikeDiv);
     contDiv.appendChild(infoDiv);
 }
 
