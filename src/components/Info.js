fdb110474911d910d26f1c71725efee019326156
Seba Soto <sebastian.pkfd@gmail.com>
2021-01-04 00:36:42 -0300

    Working on the interface

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,20 +2,33 @@ import React from 'react';
 
 const Info = (props) => {
     const {weather} = props;
+
+    let place;
+    if(weather.country === 'United States of America') {
+        place = <h3>{weather.city} - USA</h3>
+    } else {
+        place = <h3>{weather.city} - {weather.country}</h3>
+    }
+
     if(weather.temp) {
         return (
             <div className="info">
-                <div classname="place-date">
-                    <h3>{weather.city}</h3>
-                    <h3>{weather.country}</h3>
-                    <h3>{weather.currentDate}</h3>
+                <div className="place-date">
+                    <div className="place">
+                        {place}
+                    </div>
+                    <div className="date">
+                        <h3>{weather.currentDate}</h3>
+                    </div>
                 </div>
                 <div className="icon-temp-condition">
                     <div className="icon-container">
                         <img src={weather.icon} alt="icon"/>
                     </div>
-                    <h3>{weather.temp}</h3>
-                    <h3>{weather.condition}</h3>
+                    <div className="temp-condition">
+                        <h2 id="temp">{weather.temp}</h2>
+                        <h3>{weather.condition}</h3>
+                    </div>
                 </div>
                 <div className="humidity-wind">
                     <h3>Humidity: {weather.humidity}</h3>
