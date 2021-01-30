46e0cc0821c0046703523c990a1fcbda266e5fcb
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-01-30 02:13:46 -0300

    Added temp option button

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Input = (props) => {
     const {handleSubmit} = props;
+    const [tempType, setTempType] = useState('C');
 
     const submitLocation = (e) => {
         e.preventDefault();
         handleSubmit(e);
     }
 
+    const changeTempType = () => {
+        (tempType === 'C') ? setTempType('F') : setTempType ('C');
+    }
+
     return (
-    <div className="input">
-        <form onSubmit={submitLocation}>
-            <input
-                type="text"
-                name="location"
-                placeholder="Search"
-            />
-        </form>
+    <div className='options'>
+        <div className="input">
+            <form onSubmit={submitLocation}>
+                <input
+                    type="text"
+                    name="location"
+                    placeholder="Search"
+                />
+            </form>
+        </div>
+        <button onClick={changeTempType}>
+            {tempType}
+        </button>
     </div>
     )
 }
